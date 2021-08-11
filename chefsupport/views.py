90f799969206fa5ea9cd304bcc521a189782d212
Peter Miller <pmiller98@berkeley.edu>
2021-08-11 05:17:39 -0700

    backend and frontend link

--- chefsupport/views.py
+++ chefsupport/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Recipe
 
 def index(request):
     return render(request, 'homescreen.html')
@@ -7,4 +8,13 @@ def recipes(request):
     return render(request, 'recipes.html')
 
 def ingredients(request):
-    return render(request, 'ingredients.html')
+    recipes = []
+    for recipe in Recipe.objects.all():
+        recipes.append({
+            'title': recipe.title,
+            'ingredients': recipe.ingredients,
+            'directions': recipe.directions,
+            'image': recipe.image
+        })
+
+    return render(request, 'ingredients.html', {'recipes': recipes})
