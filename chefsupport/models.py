859683f4029fd18401123150474f86c1266ffa53
Nitya Krishnakumar <nitya.krish09@gmail.com>
2021-08-10 22:46:06 -0700

    made Recipe model

--- chefsupport/models.py
+++ chefsupport/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    ingredients = models.JSONField(default=list)
+    directions = models.JSONField(default=list)
+    image = models.URLField()
